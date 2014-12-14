77a1a95c3df5d5862901a38e4431e229f576b6b0
Marc Boanas <marcjohnboanas@gmail.com>
2014-12-14 18:11:44 +0000

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users
   
+  #Account Activation
+  resources :account_activations, only: [:edit]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
