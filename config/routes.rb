33e92ae70106e8d95b85592513c7a7d0f212b9bd
Marc Boanas <marcjohnboanas@gmail.com>
2014-12-15 11:24:28 +0000

    heroku sendgrid config production

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   #Account Activation
   resources :account_activations, only: [:edit]
   
+  #Password Resets
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
